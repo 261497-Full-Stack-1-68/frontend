a2f47d89bb007eb17b70521c60249be1ea06b157
Autsada Wiriya <autsadawiriya@gmail.com>
2025-08-06 18:07:34 +0700

    Refactor backend URL in API rewrites
    Introduced API_URL constant with a default value to simplify backend URL configuration in Next.js rewrites. This improves maintainability and ensures a fallback to localhost if the environment variable is not set.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.URL_BACKEND || "http://localhost:3000";
+
 const nextConfig: NextConfig = {
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/rewrites
   async rewrites() {
     return [
       {
         source: "/api/:path*", // ที่ frontend เรียก
-        destination: `${process.env.URL_BACKEND}/:path*`, // ไปที่ backend จริง
+        destination: `${API_URL}/:path*`,
       },
     ];
   },
