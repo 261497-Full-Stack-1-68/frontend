37005bc1eb318fea4a17e191777cccd536aef2dc
Autsada Wiriya <autsadawiriya@gmail.com>
2025-08-06 18:48:57 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const API_URL = process.env.URL_BACKEND || "http://localhost:3000";
-
 const nextConfig: NextConfig = {
   // https://nextjs.org/docs/app/api-reference/config/next-config-js/rewrites
   async rewrites() {
     return [
       {
         source: "/api/:path*", // ที่ frontend เรียก
-        destination: `${API_URL}/:path*`,
+        destination: "http://g09-backend:3000/:path*", // ไปที่ backend จริง
       },
     ];
   },
